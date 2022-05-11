db342e00caf73516b4469ac8cf91c3f8d556e8b1
joseEpa1 <105013183+joseEpa1@users.noreply.github.com>
2022-05-11 13:05:42 -0300

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -118,7 +118,24 @@ class CpuStrums extends Option
 	}
 
 }
+class CustomControls extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.switchState(new options.CustomControlsState());
+		return true;
+	}
+	private override function updateDisplay():String
+	{
+		return "controls";
+	}
 
+}
 class GraphicLoading extends Option
 {
 	public function new(desc:String)
